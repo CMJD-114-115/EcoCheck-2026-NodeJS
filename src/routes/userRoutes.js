42ed744df097c4093febd30e59080bce930131da
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-23 12:59:29 +0530

    Get all users - UserRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,31 @@
 const express = require("express")
 const router = express.Router()
-const userUrl  = "/users"
-const userService = ""
+const userUrl = "/users"
+const userService = require("../service/userService")
 const User = require("../model/userModel")
 
-router.get(userUrl, (req,res)=>{
-    // User service
+router.get(userUrl, async (req, res) => {
+    try {
+        const allUsers = await serService.getAllUsers()
+        const users = allUsers.map(user => ({
+            userId: user.userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            password: user.password,
+            role: user.role
+        }));
+        console.log(users)
+        res.status(200).json(users)
+    } catch (err) {
+        console.error(err)
+    }
+
 
 })
 
 router.post(userUrl, (req, res) => {
-    // User service
+   
 
 })
 
