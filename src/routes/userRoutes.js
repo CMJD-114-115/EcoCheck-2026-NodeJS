bc15b9a61dcae0c633da623c9ff8d60fad7077bd
ThanuraSilva <thanurasilva1989@gmail.com>
2026-08-23 12:25:03 +0530

    Add user routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const router = express.Router()
+const userUrl  = "/users"
+const userService = ""
+const User = require("../model/userModel")
+
+router.get(userUrl, (req,res)=>{
+    // User service
+
+})
+
+router.post(userUrl, (req, res) => {
+    // User service
+
+})
+
+router.patch(userUrl, (req, res) => {
+    // User service
+
+})
+
+router.delete(userUrl, (req, res) => {
+    // User service
+
+})
+
+module.exports = router
